99b0f2ec4ded5b1aea4b154c81d70f6eedec11de
Tên utschuichien <utprolaban@gmail.com>
2026-02-17 00:59:00 +0700

    fix-cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: ['http://localhost:5173', 'https://job-board-eta-rosy.vercel.app/'],
+        origin: ['http://localhost:5173', 'https://job-board-eta-rosy.vercel.app'],
         credentials: true,
     }),
 );
