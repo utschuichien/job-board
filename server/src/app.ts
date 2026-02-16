70ade66c8fdbf16b389250ca3a4b68644ea8a985
Tên utschuichien <utprolaban@gmail.com>
2026-02-16 23:41:09 +0700

    hehe

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,7 +19,12 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+    cors({
+        origin: ['http://localhost:5173', 'https://job-board-eta-rosy.vercel.app/'],
+        credentials: true,
+    }),
+);
 app.use(helmet());
 app.use(morgan('dev'));
 
